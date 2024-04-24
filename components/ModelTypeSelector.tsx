03518800750e381cb8148b7200511e9e081c24f8
ikhsangama <ikhsangama@gmail.com>
2024-04-24 23:52:43 +0700

    chore: radio group edit

--- components/ModelTypeSelector.tsx
+++ components/ModelTypeSelector.tsx
@@ -36,21 +36,6 @@ export function ModelTypeSelector() {
           Woman
         </Label>
       </div>
-      <div>
-        <RadioGroupItem
-          value="person"
-          id="person"
-          className="peer sr-only"
-          aria-label="person"
-        />
-        <Label
-          htmlFor="person"
-          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-        >
-          <FaRainbow className="mb-3 h-6 w-6" />
-          Unisex
-        </Label>
-      </div>
     </RadioGroup>
   );
 }
